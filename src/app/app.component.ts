0a97de93578d36936cdc051dbb6fe1960d873472
Tom Steele <tsteele@madisoncollege.edu>
2018-09-15 15:42:46 -0500

    Opps, forgot these files...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'my-root-component',
+  templateUrl: './app.html',
+  // template: `
+  //   <h1>Foo</h1>
+  //   <ul>
+  //     <li>Bar</li>
+  //   </ul>
+  // `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myapp';
+  mytitle = 'myapp123456';
+
+
+   quizzes = [
+    "Quiz 1"
+    , "Quiz 2"
+    , "Quiz 3"
+  ];
+
+  // addFunnyQuiz() {
+  //   window.alert('sdgsgf');  
+  // }
+
+  //addFunnyQuiz = () => window.alert('asdf');
+
+  //addFunnyQuiz = () => this.quizzes.push("Funny Quiz");
+
+  addFunnyQuiz = () => this.quizzes = ["Modern Funny Quiz", ...this.quizzes];
+
+  //addQuiz = () => window.alert(this.newQuizName);
+
+  addQuiz = () => {
+    console.log(this.newQuizName);
+    this.quizzes = [...this.quizzes, this.newQuizName];
+    this.newQuizName = "";
+  };
+
+
+  newQuizName = "nonsense";
+
 }

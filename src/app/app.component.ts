686031b220a4f03ffb20337af838aa805d6db15c
Sylvie <sschuresko@madisoncollege.edu>
2018-09-20 18:52:28 -0500

    Converted quizzes to objects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,10 +15,17 @@ export class AppComponent {
   mytitle = 'Sylvie\'s app';
 
 
-   quizzes = [
+/*    quizzes = [
     "Quiz 1"
     , "Quiz 2"
     , "Quiz 3"
+  ]; */
+
+  // let's make quizzes an array of objects instead of strings
+  quizzes = [
+    { name: "Quiz 1" }
+    , { name: "Quiz 2" }
+    , { name: "Quiz 3" }
   ];
 
   // addFunnyQuiz() {
@@ -29,13 +36,13 @@ export class AppComponent {
 
   //addFunnyQuiz = () => this.quizzes.push("Funny Quiz");
 
-  addFunnyQuiz = () => this.quizzes = ["Modern Funny Quiz", ...this.quizzes];
+  addFunnyQuiz = () => this.quizzes = [{name: "Modern Funny Quiz"}, ...this.quizzes];
 
   //addQuiz = () => window.alert(this.newQuizName);
 
   addQuiz = () => {
     console.log(this.newQuizName);
-    this.quizzes = [...this.quizzes, this.newQuizName];
+    this.quizzes = [...this.quizzes, {name: this.newQuizName}];
     this.newQuizName = "";
   };
 

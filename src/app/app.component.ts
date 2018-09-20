b8f1cce8457cfeafcc1b9d9726382ee32ecf4768
Isaac Lemke <cpu.ikemike@gmail.com>
2018-09-20 18:52:52 -0500

    Rewrote addQuiz function, updated quiz to an object.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,11 +15,11 @@ export class AppComponent {
   mytitle = 'myapp123456';
 
 
-   quizzes = [
-    "Quiz 1"
-    , "Quiz 2"
-    , "Quiz 3"
-  ];
+  //  quizzes = [
+  //   "Quiz 1"
+  //   , "Quiz 2"
+  //   , "Quiz 3"
+  // ];
 
   // addFunnyQuiz() {
   //   window.alert('sdgsgf');  
@@ -33,13 +33,26 @@ export class AppComponent {
 
   //addQuiz = () => window.alert(this.newQuizName);
 
+  /*
   addQuiz = () => {
     console.log(this.newQuizName);
     this.quizzes = [...this.quizzes, this.newQuizName];
     this.newQuizName = "";
   };
-
-
   newQuizName = "nonsense";
+  */
+
+  /* ILEMKE: New code to add a delete button for new entries*/
+  quizzes = [
+    {name: "Quiz 1"},
+    {name: "Quiz 2"},
+    {name: "Quiz 3"}
+  ];
+
+  addQuiz = () => {
+    this.quizzes = [...this.quizzes, {name: this.newQuizName}];
+  }
+
+
 
 }

f2519ef233f07d9905e9defea7431c35d6d6d781
smnkdrnk <34699456+smnkdrnk@users.noreply.github.com>
2018-09-20 18:54:26 -0500

    Comment cleanup and strings replaced by objects
    Converted String array to an object array.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,39 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-root-component',
   templateUrl: './app.html',
-  // template: `
-  //   <h1>Foo</h1>
-  //   <ul>
-  //     <li>Bar</li>
-  //   </ul>
-  // `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   mytitle = 'myapp123456';
 
 
-   quizzes = [
-    "Quiz 1"
-    , "Quiz 2"
-    , "Quiz 3"
+  quizzes = [
+    { name: "Quiz 1" }
+    , { name: "Quiz 2" }
+    , { name: "Quiz 3" }
   ];
 
-  // addFunnyQuiz() {
-  //   window.alert('sdgsgf');  
-  // }
 
-  //addFunnyQuiz = () => window.alert('asdf');
+  addFunnyQuiz = () => this.quizzes = [{ name: "Modern Funny Quiz" }, ...this.quizzes];
 
-  //addFunnyQuiz = () => this.quizzes.push("Funny Quiz");
-
-  addFunnyQuiz = () => this.quizzes = ["Modern Funny Quiz", ...this.quizzes];
-
-  //addQuiz = () => window.alert(this.newQuizName);
 
   addQuiz = () => {
     console.log(this.newQuizName);
-    this.quizzes = [...this.quizzes, this.newQuizName];
+    this.quizzes = [...this.quizzes, { name: this.newQuizName }];
     this.newQuizName = "";
   };
 

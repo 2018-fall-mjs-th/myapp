9dbc1120fbfdee888150f6405ebdbd0e768135cd
Robert Collier <rcollier@madisoncollege.edu>
2018-09-20 18:59:31 -0500

    gotten the buttons working
    The buttons show/hide as proscribed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,10 +15,16 @@ export class AppComponent {
   mytitle = 'myapp123456';
 
 
-   quizzes = [
-    "Quiz 1"
-    , "Quiz 2"
-    , "Quiz 3"
+  //  quizzes = [
+  //   "Quiz 1"
+  //   , "Quiz 2"
+  //   , "Quiz 3"
+  // ];
+
+  quizzes = [
+    {name:  "Quiz 1", canDelete: false}
+    , {name:  "Quiz 2", canDelete: false}
+    , {name:  "Quiz 3", canDelete: false}
   ];
 
   // addFunnyQuiz() {
@@ -29,13 +35,13 @@ export class AppComponent {
 
   //addFunnyQuiz = () => this.quizzes.push("Funny Quiz");
 
-  addFunnyQuiz = () => this.quizzes = ["Modern Funny Quiz", ...this.quizzes];
+  addFunnyQuiz = () => this.quizzes = [{name: "Modern Funny Quiz", canDelete: false}, ...this.quizzes];
 
   //addQuiz = () => window.alert(this.newQuizName);
 
   addQuiz = () => {
     console.log(this.newQuizName);
-    this.quizzes = [...this.quizzes, this.newQuizName];
+    this.quizzes = [...this.quizzes, {name: this.newQuizName, canDelete: true}];
     this.newQuizName = "";
   };
 

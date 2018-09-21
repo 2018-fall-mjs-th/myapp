69b20b513b15287dcbf2bc952617c7fc31837347
tsteelematc <tsteele@madisoncollege.edu>
2018-09-20 19:23:59 -0500

    Isaac asked how delete by name...
    This was my VanillaJS naive delete..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,12 +43,12 @@ export class AppComponent {
   newQuizName = "nonsense";
 
   deleteQuiz(quizToDelete) {
-    //console.log(q);
+    console.log(quizToDelete);
 
     // Transform the view model... In other words, reinitialize 
     // this.quizzes to a new array without the quiz to delete. Which
     // is passed in as an argument or parameter, who cares...
-    this.quizzes = this.quizzes.filter(x => x !== quizToDelete);
+    this.quizzes = this.quizzes.filter(x => x.name !== quizToDelete.name);
   }
 
 }
